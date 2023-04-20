ca7317f706e2f491b194e9a9ed4457361acd6714
@bunabuna <bunabuna@amazon.com>
2023-04-20 09:35:11 -0400

    initial commit

--- bin/cdk-ci-cd-all.ts
+++ bin/cdk-ci-cd-all.ts
@@ -1,21 +1,20 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkCiCdAllStack } from '../lib/cdk-ci-cd-all-stack';
+import {VPCStack} from "../lib/vpc";
+
+import console = require("console");
 
 const app = new cdk.App();
-new CdkCiCdAllStack(app, 'CdkCiCdAllStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const account = app.node.tryGetContext("account");
+const region = app.node.tryGetContext("region");
+const tagname = app.node.tryGetContext("tagName");
+const tagvalue = app.node.tryGetContext("tagValue");
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const myVPCStack = new VPCStack(app, "myVPCStack", {
+  env: { account: account, region: region },
+  stackName: "ohdsi-VPCStack"
 });
+cdk.Tags.of(myVPCStack).add(tagname,tagvalue);
