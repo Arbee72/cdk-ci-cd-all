e426d738935520333bad69338749b814db69dcd1
@bunabuna <bunabuna@amazon.com>
2023-04-25 13:30:49 -0400

    test5

--- bin/cdk-ci-cd-all.ts
+++ bin/cdk-ci-cd-all.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import {VPCStack} from "../lib/vpc";
 import {CICDPipelineStack} from "../lib/ci-cd-pipeline";
 
 import console = require("console");
@@ -13,12 +12,6 @@ const region = app.node.tryGetContext("region");
 const tagname = app.node.tryGetContext("tagName");
 const tagvalue = app.node.tryGetContext("tagValue");
 
-
-//const myVPCStack = new VPCStack(app, "myVPCStack", {
-  //env: { account: account, region: region },
-  //stackName: "cicd-VPCStack"
-//});
-//cdk.Tags.of(myVPCStack).add(tagname,tagvalue);
 
 const myCICDPipelineStack = new CICDPipelineStack(app, "myCICDPipelineStack", {
   env: { account: account, region: region },
